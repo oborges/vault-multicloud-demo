becebf2990d52aeac36f996db3b2566ae99e4644
Olavo Borges <olavoborges@MacBook-Pro-de-Olavo.local>
2025-07-29 13:54:14 -0300

    wip

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,4 +1,9 @@
+############################################
+# Terraform CLI & required providers
+############################################
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,20 +11,28 @@ terraform {
     }
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "~> 1.60"
+      version = ">= 1.60.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 }
 
+############################################
+# Provider configurations
+############################################
 provider "azurerm" {
   features {}
+  subscription_id = var.azure_subscription_id
+  tenant_id       = var.azure_tenant_id
 }
 
-provider "ibm" {}
+# Single IBM Cloud provider (region set via variable)
+provider "ibm" {
+  region = var.ibm_region
+}
 
-# AKS cluster (Azure)
+############################################
+# AKS (Azure Kubernetes Service) module
+############################################
 module "azure_aks" {
   source              = "../../modules/azure-aks"
   resource_group_name = var.azure_resource_group_name
@@ -28,33 +41,60 @@ module "azure_aks" {
   dns_prefix          = var.aks_dns_prefix
   node_count          = var.aks_node_count
   vm_size             = var.aks_vm_size
-  tags                = var.common_tags
 }
 
-# ROKS cluster (IBM Cloud)
+############################################
+# ROKS (IBM Cloud OpenShift) module
+############################################
 module "ibm_roks" {
-  source             = "../../modules/ibm-roks"
-  cluster_name       = var.roks_cluster_name
-  region             = var.ibm_region
-  resource_group_id  = var.ibm_resource_group_id
-  flavor             = var.roks_flavor
-  vpc_id             = var.ibm_vpc_id
-  zones              = var.ibm_zones
-  kube_version       = var.roks_kube_version
-  worker_count       = var.roks_worker_count
-  tags               = var.common_tags
+  source  = "github.com/oborges/ibmcloud-infra-terraform//modules/roks?ref=main"
+  ibmcloud_api_key  = var.ibmcloud_api_key
+  region            = var.ibm_region
+  cluster_name      = var.roks_cluster_name
+  vpc_name          = var.ibm_vpc_name
+  zones             = var.ibm_zones          # set(string)
+  subnet_ids        = var.ibm_subnet_ids     # map(zone => subnet_id)
+  kube_version      = var.roks_kube_version
+  worker_flavor     = var.roks_flavor
+  workers_per_zone  = var.roks_worker_count
 }
 
-# Virtual Machine (IBM Cloud)
+############################################
+# IBM Cloud Virtual Server (VSI) module
+############################################
 module "ibm_vm" {
-  source        = "../../modules/ibm-vm"
-  instance_name = var.vm_instance_name
-  image_id      = var.vm_image_id
-  profile       = var.vm_profile
-  zone          = var.ibm_zone
-  vpc_id        = var.ibm_vpc_id
-  subnet_id     = var.ibm_subnet_id
-  ssh_key_id    = var.ibm_ssh_key_id
-  tags          = var.common_tags
+  source  = "github.com/oborges/ibmcloud-infra-terraform//modules/vsi?ref=main"
+  ibmcloud_api_key       = var.ibmcloud_api_key
+  region                 = var.ibm_region
+  instance_name          = var.vm_instance_name
+  zone                   = var.ibm_zone
+  vpc_name               = var.ibm_vpc_name           # module resolves VPC by name
+  subnet_id              = var.ibm_subnet_id
+  image_name             = var.vm_image_name
+  profile                = var.vm_profile
+  existing_ssh_key_name  = var.ibm_ssh_key_name
+  attach_floating_ip     = var.vm_attach_fip
+  allowed_ssh_cidr       = var.vm_allowed_ssh_cidr
+  environment            = var.environment
 }
+
+############################################
+# Helpful outputs
+############################################
+output "aks_kube_config" {
+  value       = module.azure_aks.kube_config
+  description = "Kubeconfig for the AKS cluster (sensitive)"
+  sensitive   = true
+}
+
+#output "roks_kube_config" {
+#  value       = module.ibm_roks.kube_config
+#  description = "Kubeconfig for the ROKS cluster (sensitive)"
+#  sensitive   = true
+#}
+
+#output "vm_primary_ip" {
+#  value       = module.ibm_vm.primary_ipv4
+#  description = "Primary IPv4 address (or FIP) of the VSI"
+#}
 
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,110 +1,149 @@
+########################################################
 # Azure variables
+########################################################
+
 variable "azure_resource_group_name" {
   type        = string
-  description = "Azure resource group name"
+  description = "Resource group in which the AKS cluster will be created"
 }
 
 variable "azure_region" {
   type        = string
-  description = "Azure region"
+  description = "Azure region (e.g. eastus, westeurope)"
 }
 
 variable "aks_cluster_name" {
   type        = string
-  description = "AKS cluster name"
+  description = "Name of the Azure Kubernetes Service (AKS) cluster"
 }
 
 variable "aks_dns_prefix" {
   type        = string
-  description = "AKS DNS prefix"
+  description = "DNS prefix for the AKS API server"
 }
 
 variable "aks_node_count" {
   type        = number
+  description = "Initial node count for the default node pool"
   default     = 3
 }
 
 variable "aks_vm_size" {
   type        = string
+  description = "VM size (SKU) for AKS worker nodes"
   default     = "Standard_DS2_v2"
 }
 
-# IBM Cloud variables
+########################################################
+# IBM Cloud variables (common)
+########################################################
+
+variable "ibmcloud_api_key" {
+  type        = string
+  sensitive   = true
+  description = "IBM Cloud API key passed down to child modules"
+}
+
 variable "ibm_region" {
   type        = string
-  description = "IBM Cloud region"
+  description = "IBM Cloud region (e.g. us-south, eu-gb)"
 }
 
-variable "ibm_resource_group_id" {
+variable "ibm_vpc_name" {
   type        = string
-  description = "IBM Cloud resource group ID"
+  description = "Name of an existing VPC used by both ROKS and the VM"
 }
 
-variable "ibm_vpc_id" {
-  type        = string
-  description = "IBM Cloud VPC ID"
-}
-
-variable "ibm_zones" {
-  type        = list(string)
-  description = "IBM Cloud zones"
-}
-
-variable "ibm_zone" {
-  type        = string
-  description = "Single zone for VM"
-}
-
-variable "ibm_subnet_id" {
-  type        = string
-  description = "Subnet ID for VM"
-}
-
-variable "ibm_ssh_key_id" {
-  type        = string
-  description = "SSH key ID for VM"
-}
-
-# ROKS specific
+########################################################
+# ROKS‑specific variables (module mirrors oborges/roks)
+########################################################
 variable "roks_cluster_name" {
   type        = string
-}
-
-variable "roks_flavor" {
-  type        = string
-  default     = "bx2.4x16"
+  description = "Name of the OpenShift (ROKS) cluster"
 }
 
 variable "roks_kube_version" {
   type        = string
-  default     = "4.14_openshift"
+  description = "OpenShift version string, e.g. 4.15_openshift (blank = latest GA)"
+  default     = ""
+}
+
+variable "roks_flavor" {
+  type        = string
+  description = "Worker node flavor (e.g. bx2.4x16)"
+  default     = "bx2.4x16"
 }
 
 variable "roks_worker_count" {
   type        = number
+  description = "Number of workers per zone"
   default     = 3
 }
 
-# VM specific
-variable "vm_instance_name" {
-  type        = string
+variable "ibm_zones" {
+  type        = set(string)
+  description = "Set of zones for the ROKS worker pool (e.g. [\"us-south-1\", \"us-south-2\", \"us-south-3\"])"
 }
 
-variable "vm_image_id" {
+variable "ibm_subnet_ids" {
+  type        = map(string)
+  description = "Map of zone name => subnet ID for each ROKS zone"
+}
+
+########################################################
+# VM‑specific variables (module mirrors oborges/vsi)
+########################################################
+variable "vm_instance_name" {
   type        = string
+  description = "Name of the virtual server instance (VSI)"
+}
+
+variable "ibm_zone" {
+  type        = string
+  description = "Single availability zone used for the VSI (e.g. us-south-1)"
+}
+
+variable "ibm_subnet_id" {
+  type        = string
+  description = "Subnet ID in which the VSI will be placed"
+}
+
+variable "vm_image_name" {
+  type        = string
+  description = "Public image name for the VSI (e.g. ibm-redhat-9-6-minimal-amd64-2)"
+  default     = "ibm-redhat-9-6-minimal-amd64-2"
 }
 
 variable "vm_profile" {
   type        = string
+  description = "Compute profile for the VSI (e.g. bx2-2x8)"
   default     = "bx2-2x8"
 }
 
-# Common tags
-variable "common_tags" {
-  type    = map(string)
-  default = {
-    environment = "dev"
-    project     = "vault-demo"
-  }
+variable "ibm_ssh_key_name" {
+  type        = string
+  description = "Name of an existing IBM Cloud SSH key to reuse (blank = auto‑generate)"
+  default     = ""
+}
+
+variable "vm_attach_fip" {
+  type        = bool
+  description = "Whether to attach a floating IP to the VSI"
+  default     = true
+}
+
+variable "vm_allowed_ssh_cidr" {
+  type        = string
+  description = "CIDR block allowed for SSH inbound traffic"
+  default     = "0.0.0.0/0"
+}
+
+########################################################
+# Common tags / environment label
+########################################################
+variable "environment" {
+  type        = string
+  description = "Environment label (dev, stage, prod)"
+  default     = "dev"
 }
 
